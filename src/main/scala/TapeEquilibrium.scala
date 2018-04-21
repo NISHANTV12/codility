823de913348d903bfac04362ffe3a08d30ae4eb6
Nishant Vishwakarma <nishantv12@gmail.com>
2018-04-21 14:23:11 +0530

    Add new solution to factor in presence of negative numbers. Can't avoid full iteration.

--- src/main/scala/TapeEquilibrium.scala
+++ src/main/scala/TapeEquilibrium.scala
@@ -1,44 +1,17 @@
 object TapeEquilibrium {
+
   def solution(a: Array[Int]): Int = {
-    val splitPoint = a.length / 2
-    val parts      = a.splitAt(splitPoint)
-
-    val equilibrium =
-      if (parts._1.sum > parts._2.sum)
-        shiftLeft(a, splitPoint)
-      else
-        shiftRight(a, splitPoint)
-
-    val tuple = a.splitAt(equilibrium)
-    Math.abs(tuple._1.sum - tuple._2.sum)
-  }
-
-  private def shiftLeft(a: Array[Int], splitPoint: Int): Int =
-    shift(a, splitPoint, validateLeftShift, _ - 1)
-
-  private def shiftRight(a: Array[Int], splitPoint: Int): Int =
-    shift(a, splitPoint, validateRightShift, _ + 1)
-
-  private def shift(
-      a: Array[Int],
-      splitPoint: Int,
-      condition: (Array[Int], Int) ⇒ Boolean,
-      shiftExpression: Int ⇒ Int
-  ): Int = {
-    if (!condition(a, splitPoint))
-      splitPoint
-    else
-      shift(a, shiftExpression(splitPoint), condition, shiftExpression)
-  }
-
-  private def validateRightShift(a: Array[Int], splitPoint: Int) = {
-    val parts = a.splitAt(splitPoint + 1)
-    parts._1.sum < parts._2.sum && splitPoint + 1 < a.length
-  }
-
-  private def validateLeftShift(a: Array[Int], splitPoint: Int) = {
-    val parts = a.splitAt(splitPoint - 1)
-    parts._1.sum > parts._2.sum && splitPoint - 1 > 0
+    def loop(a: Array[Int], splitPoint: Int, equilibrium: Int): Int = splitPoint match {
+      case 1 ⇒ equilibrium
+      case _ ⇒
+        val tuple = a.splitAt(splitPoint)
+        val diff  = Math.abs(tuple._1.sum - tuple._2.sum)
+        if (diff < equilibrium)
+          loop(a, splitPoint - 1, diff)
+        else
+          loop(a, splitPoint - 1, equilibrium)
+    }
+    loop(a, a.length - 1, Int.MaxValue)
   }
 
 }
--- src/test/scala/TapeEquilibriumTest.scala
+++ src/test/scala/TapeEquilibriumTest.scala
@@ -1,10 +1,23 @@
 import org.scalatest.{FunSuite, Matchers}
 
 class TapeEquilibriumTest extends FunSuite with Matchers {
-  test("testSolution") {
+  test("test Tape Equilibrium") {
+    TapeEquilibrium.solution(Array(3, 1, 2, 4, 3, 6, 7)) shouldBe 0
+  }
+
+  test("test Tape Equilibrium with negative numbers") {
+    TapeEquilibrium.solution(Array(3, 1, 2, 4, -3)) shouldBe 1
+  }
+
+  test("test Tape Equilibrium with very high values at edge") {
     TapeEquilibrium.solution(Array(3, 1, 2, 4, 3, 6, 1000)) shouldBe 981
+    TapeEquilibrium.solution(Array(3000, 1, 2, 4, 3, 6, 1)) shouldBe 2985
+  }
+
+  test("test Tape Equilibrium with uniform increasing and decreasing numbers") {
     TapeEquilibrium.solution(Array(1, 2, 3, 4, 5, 4, 3, 2, 1)) shouldBe 5
-    TapeEquilibrium.solution(Array(3, 1, 2, 4, 3)) shouldBe 1
+    TapeEquilibrium.solution(Array(1, 2, 3, 4, 5)) shouldBe 3
+    TapeEquilibrium.solution(Array(5, 4, 3, 2, 1)) shouldBe 3
   }
 
 }
