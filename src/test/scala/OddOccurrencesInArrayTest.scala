521be326eea1bba5d64489e5ee52c917561b8508
Nishant Vishwakarma <nishantv12@gmail.com>
2018-04-21 13:04:16 +0530

    Added tests for Odd occurrency in Array

--- src/test/scala/OddOccurrencesInArrayTest.scala
+++ src/test/scala/OddOccurrencesInArrayTest.scala
@@ -1,8 +1,16 @@
 import org.scalatest.{FunSuite, Matchers}
 
 class OddOccurrencesInArrayTest extends FunSuite with Matchers {
-  test("testSolution") {
+  test("test Odd occurrences") {
     OddOccurrencesInArray.solution(Array(1, 1, 1, 1, 4)) shouldBe 4
+  }
+
+  test("test Odd occurrences with more duplicate numbers") {
+    OddOccurrencesInArray.solution(Array(1, 2, 13, 19, 4, 4, 1, 13, 2)) shouldBe 19
+  }
+
+  test("test Odd occurrences with single number") {
+    OddOccurrencesInArray.solution(Array(1)) shouldBe 1
   }
 
 }
