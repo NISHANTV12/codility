df95a9c7266571a4cae72b6441af5182069c0a32
Nishant Vishwakarma <nishantv12@gmail.com>
2018-04-21 13:03:30 +0530

    Added tests for BinaryGap

--- src/test/scala/BinaryGapTest.scala
+++ src/test/scala/BinaryGapTest.scala
@@ -5,4 +5,13 @@ class BinaryGapTest extends FunSuite with Matchers {
   test("binary gap") {
     solution(129) shouldBe 6
   }
+
+  test("binary gap on large number") {
+    solution(129129129) shouldBe 2
+  }
+
+  test("binary gap on negative number") {
+    solution(-129) shouldBe 1
+  }
+
 }
