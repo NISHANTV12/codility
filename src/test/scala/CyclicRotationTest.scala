5b85af9c6c719b8cae27a2d4f1226f7ef261a209
Nishant Vishwakarma <nishantv12@gmail.com>
2018-04-21 13:03:47 +0530

    Added tests for cyclic rotation

--- src/test/scala/CyclicRotationTest.scala
+++ src/test/scala/CyclicRotationTest.scala
@@ -1,7 +1,16 @@
 import org.scalatest.{FunSuite, Matchers}
 
 class CyclicRotationTest extends FunSuite with Matchers {
-  test("testSolution") {
+  test("test Cyclic Rotation") {
     CyclicRotation.solution(Array(3, 8, 9, 7, 6), 3) shouldBe List(9, 7, 6, 3, 8)
   }
+
+  test("test Cyclic Rotation with negative numbers") {
+    CyclicRotation.solution(Array(-3, 8, 9, -7, 6), 3) shouldBe List(9, -7, 6, -3, 8)
+  }
+
+  test("test Cyclic Rotation with duplicate numbers") {
+    CyclicRotation.solution(Array(-3, 8, 9, -7, 6, 8, 1), 4) shouldBe List(-7, 6, 8, 1, -3, 8, 9)
+  }
+
 }
